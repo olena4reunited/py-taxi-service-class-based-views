cd80d9514bfa4b02de6195d6d84c02bb7c20b1d4
olena4reunited <olenaduringintermission@gmail.com>
2024-09-09 20:58:36 +0300

    build: create additional pages for list view of all models and detailed view for objects; fix nav bar to navigate throw pages

--- taxi/views.py
+++ taxi/views.py
@@ -1,15 +1,49 @@
+# views.py
 from django.shortcuts import render
-
+from django.views import generic
 from taxi.models import Driver, Car, Manufacturer
 
 
 def index(request):
-    """View function for the home page of the site."""
-
     context = {
         "num_drivers": Driver.objects.count(),
         "num_cars": Car.objects.count(),
         "num_manufacturers": Manufacturer.objects.count(),
     }
-
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    context_object_name = "manufacturer_list"
+    template_name = "taxi/manufacturer_list.html"
+    paginate_by = 5
+    ordering = "name"
+
+
+class CarListView(generic.ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer")
+    context_object_name = "car_list"
+    template_name = "taxi/car_list.html"
+    paginate_by = 5
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+    context_object_name = "car_detail"
+    template_name = "taxi/car_detail.html"
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    context_object_name = "driver_list"
+    template_name = "taxi/driver_list.html"
+    paginate_by = 5
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
+    context_object_name = "driver_detail"
+    template_name = "taxi/driver_detail.html"
